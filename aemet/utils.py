6c0f9067f07f0fa405126b12aac09fc5055b9e67
Pablo Moreno <pablomoreno.inf@gmail.com>
2017-09-06 18:24:02 +0200

    fix: data files and csv_to_json function

--- aemet/utils.py
+++ aemet/utils.py
@@ -7,15 +7,12 @@ def csv_to_json(csv_file, json_file):
         fields = f.readline().strip().split(',')
         reader = csv.reader(f)
         data = list(reader)
-    array = []
-    for element in data:
-        array.append(dict(zip(fields, element)))
-    del array[0]
+    array = [dict(zip(fields, element)) for element in data]
     with open(json_file, 'w', encoding='utf-8') as f:
         f.write(json.dumps(array, indent=4))
 
 if __name__ == '__main__':
     csv_to_json(
-        os.path.join('..', 'data', 'estaciones_contaminacion.csv'),
-        os.path.join('..', 'data', 'estaciones_contaminacion.json'),
+        os.path.join('..', 'data', 'municipios.csv'),
+        os.path.join('..', 'data', 'municipios.json'),
     )
