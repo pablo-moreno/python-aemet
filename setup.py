b5f076f9b35471ff185b463c76ac53655080a27e
Pablo Moreno <pablomoreno.inf@gmail.com>
2017-09-07 17:46:57 +0200

    little changes

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-A setuptools based setup module.
+Python AEMET Library
 
 See:
 https://packaging.python.org/en/latest/distributing.html
@@ -29,7 +29,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.17.9.04.dev',
+    version='0.17.9.07.dev',
 
     description=description,
     long_description=long_description,
