484d9cc511f049a8d06008f9c958f8fe0007d231
dargueso <d.argueso@uib.es>
2020-06-28 12:51:30 +0200

    Actualizado Contributors y setup.py
    La versión del setup.py vuelve a ser dinámica

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.3.1',
+    version='<RELEASE_VERSION>',
 
     description=description,
     long_description=long_description,
