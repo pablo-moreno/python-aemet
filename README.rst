3664d605c82ea6df40fafbffa886a86514039627
Pablo Moreno <pablomoreno.inf@gmail.com>
2020-07-05 23:55:36 +0200

    Fix README.rst

--- README.rst
+++ README.rst
@@ -3,10 +3,8 @@ Python AEMET
 ============
 .. image:: https://img.shields.io/pypi/v/python-aemet
    :target: https://pypi.org/project/python-aemet
-   
 .. image:: https://img.shields.io/pypi/dm/python-aemet
    :target: https://pypi.org/project/python-aemet
-   
 .. image:: https://github.com/pablo-moreno/python-aemet/workflows/Upload%20Python%20Package/badge.svg
    :target: https://github.com/pablo-moreno/python-aemet/actions
 
@@ -15,7 +13,7 @@ Librería cliente de la API de datos de AEMET. Permite obtener y manejar la info
 La información que recoge y utiliza esta librería es propiedad de la Agencia Estatal de Meteorología.
 
 Instalación
-===========
+============
 Utiliza pip para instalar la librería:
 
 .. code-block:: bash
@@ -32,7 +30,7 @@ https://opendata.aemet.es/centrodedescargas/obtencionAPIKey
 
 
 Usar la librería
-================
+=================
 
 La clase principal de la librería es la clase Aemet.
 
@@ -46,6 +44,6 @@ Instancia un objeto con la API key y tendrás acceso a todos los métodos.
 Para más información, revisa la documentación
 
 ¿Dudas, sugerencias?
-====================
+=====================
 
 Para cualquier duda, sugerencia o mejora, siéntete libre de abrir una issue en el repositorio. No contesto a correos.
--- setup.py
+++ setup.py
@@ -3,100 +3,45 @@ Python AEMET Library
 See:
 https://packaging.python.org/en/latest/distributing.html
 """
-
-# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
 from os import path, environ
-here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'description.txt'), encoding='utf-8') as f:
-    description = f.read()
+here = path.abspath(path.dirname(__file__))
+description = 'Cliente de la API de datos de AEMET'
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst')) as f:
     long_description = f.read()
 
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+with open(path.join(here, 'requirements.txt')) as f:
     requirements = f.read().splitlines()
 
 setup(
     name='python-aemet',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     version='<RELEASE_VERSION>',
-
     description=description,
     long_description=long_description,
-    long_description_content_type='text/markdown',
-
-    # The project's main homepage.
     url='https://github.com/pablo-moreno/python-aemet',
-
-    # Author details
     author='Pablo Moreno',
     author_email='pablomoreno.inf@gmail.com',
-
-    # Choose your license
     license='MIT',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 5 - Production/Stable',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
-
-        # OS
         'Operating System :: OS Independent',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-
-    # What does your project relate to?
     keywords='aemet weather api spain opendata',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     include_package_data=True,
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     install_requires=[requirements],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
     extras_require={
         'dev': ['check-manifest'],
         'test': ['coverage'],
     },
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
             'aemet=aemet:main',
