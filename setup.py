b7f904ca2d00baf1d3a6d5967cf785b15fe2837d
Pablo Moreno <pablomoreno.inf@gmail.com>
2023-03-27 10:50:14 +0200

    v0.4.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(path.join(here, 'requirements.txt')) as f:
 
 setup(
     name='python-aemet',
-    version='0.4.0',
+    version='0.4.1',
     description=description,
     long_description=long_description,
     url='https://github.com/pablo-moreno/python-aemet',
